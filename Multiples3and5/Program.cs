ae7d6f5697d75fd0468bb03c5436766d5b333130
wcoltonweeks <wcoltonweeks@outlook.com>
2021-04-05 17:06:17 -0500

    Completed_multiplesof3and5

--- Multiples3and5/Program.cs
+++ Multiples3and5/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Multiples3and5
 {
@@ -10,6 +8,35 @@ namespace Multiples3and5
     {
         static void Main(string[] args)
         {
+            //If we list all the natural numbers below 10
+            //that are multiples of 3 or 5, we get 3, 5,
+            //6 and 9.The sum of these multiples is 23.
+
+            //Find the sum of all the multiples of 3 or 5 below 1000 and print it out to the console.
+
+
+            //Solution
+            List<int> Mult3and5 = new List<int>(); //Create a List to store all the multiples
+
+            for (int i = 0; i < 1000; i = i + 3)
+            {
+                Mult3and5.Add(i);                  //Iterated through all the multiples of 3 below 1000 and adds the values to the List
+            }
+            for (int i = 0; i < 1000; i = i + 5)
+            {
+                if (!Mult3and5.Contains(i))
+                {
+                    Mult3and5.Add(i);              //Iterated through all the multiples of 5 below 1000. Only added values the List did not already contain
+                }
+            }
+            
+            //foreach (int item in Mult3and5)
+            //{
+            //    Console.WriteLine(item);         //Code for testing the list values
+            //}
+
+            int sum = Mult3and5.Sum();             //Total all the values in the list. the Sum() comes from System.Linq
+            Console.WriteLine(sum);                //Print the result
 
         }
     }
